9cdca17478daf26f829db536f76763ec0c99d4b4
JaP55 <jpereira@tycoint.com>
2016-09-22 11:58:23 -0700

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,20 +1,18 @@
-package {'memcached':
-  ensure => present,
-  before => File['/etc/sysconfig/memcached.conf']
-}
+class memcached {
+  package {'memcached':
+    ensure => present,
+    before => File['/etc/sysconfig/memcached.conf']
+  }
 
-file {'/etc/sysconfig/memcached.conf':
-  ensure => file,
-  content => "PORT="11211",
-  content => "USER="memcached"",
-  content => "MAXCONN="96"",
-  content => ""32"",
-  content => "OPTIONS=""",
-  source => 'puppet:///modules/memcached/memcached.conf',
-  notify => Service['memcached'],
-}
+  file {'/etc/sysconfig/memcached.conf':
+    ensure => file,
+    path ,
+    source => 'puppet:///modules/memcached/memcached.conf',
+    requier => ,
+    }
 
-service {'memcached':
-  ensure => running,
-  enable => true,
+  service {'memcached':
+    ensure => running,
+    enable => true,
+  }
 }
