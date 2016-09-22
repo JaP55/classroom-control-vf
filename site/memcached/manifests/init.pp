af4460a6fa686f628f85c4140bea67d29c4939b2
JaP55 <jpereira@tycoint.com>
2016-09-22 14:59:00 -0700

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,7 +1,7 @@
 class memcached {
   package {'memcached':
     ensure => present,
-    before => File['/etc/sysconfig/memcached']
+    before => File['/etc/sysconfig/memcached'],
   }
 
   file {'/etc/sysconfig/memcached':
