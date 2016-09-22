467afd9c745e87597f673edea27053fa9e863beb
JaP55 <jpereira@tycoint.com>
2016-09-22 14:37:04 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,15 +2,21 @@ class nginx {
   package {'nginx':
     ensure => present,
     before => File['/etc/nginx/nginx.conf']
+    before => File['/etc/nginx/conf.d/default.conf']
   }
-
   file {'/etc/nginx/nginx.conf':
     ensure => file,
     path => '/etc/nginx/nginx.conf',
     source => 'puppet:///modules/nginx/files/nginx.conf',
-    require => Package['nginx'],
-    }
-
+    require => Package['nginx']
+  }
+    
+  file {'/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    path => '/etc/nginx/conf.d/default.conf',
+    source => 'puppet:///modules/nginx/files/nginx.conf',
+    require => Package['nginx']
+  }
   service {'ngnix':
     ensure => running,
     enable => true,
