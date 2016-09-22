e031790944b1840785f891a24b28f35d199e3a3c
JaP55 <jpereira@tycoint.com>
2016-09-22 14:41:45 -0700

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -7,7 +7,7 @@ class memcached {
   file {'/etc/sysconfig/memcached':
     ensure => file,
     path => '/etc/sysconfig/memcached.conf',
-    source => 'puppet:///modules/memcached/files/sysconfig/memcached',
+    source => 'puppet:///modules/memcached/sysconfig/memcached',
     require => Package['memcached'],
     }
 
