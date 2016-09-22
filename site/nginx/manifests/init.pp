5e7c944c66dfcf52401b773e27652c404735f1fc
JaP55 <jpereira@tycoint.com>
2016-09-22 15:07:34 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,14 +6,24 @@ class nginx {
   file {'/etc/nginx/nginx.conf':
     ensure => file,
     path => '/etc/nginx/nginx.conf',
+    onwer => 'root',
+    group => 'root',
+    mode  => '0664',
     source => 'puppet:///modules/nginx/files/nginx.conf',
     require => Package['nginx'],
     }
   file {'/etc/nginx/conf.d/default.conf':
     ensure => file,
     path => '/etc/nginx/conf.d/default.conf',
+    onwer => 'root',
+    group => 'root',
+    mode  => '0664',
     source => 'puppet:///modules/nginx/files/default.conf',
     require => Package['nginx'],
+    }
+  file { 'index.html':
+    ensure => file,
+    source => 'puppet:///modules/nginx/files/index.html',
     }
   service {'ngnix':
     ensure => running,
